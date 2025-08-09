c1821952641a1098cc54b6a1515b2c9b9eaaf007
Suyog Tandel <suyogtandel@proton.me>
2025-08-09 11:58:46 +0530

    chore: update workflows, metadata, and packaging for v1.1.6 release
    - Bumped application version to 1.1.6.
    - Set `fail-fast: true` in build and test workflows.
    - Updated Flatpak metadata with new version tag, archive URLs, and checksums.
    - Refreshed screenshot URLs and added release metadata for v1.1.6.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,7 +50,7 @@ compose.desktop {
 				TargetFormat.Rpm
 			)
 			packageName = "pomolin"
-			packageVersion = "1.1.5"
+			packageVersion = "1.1.6"
 			description = "A simple Pomodoro App written in Kotlin. Focus on what matters! "
 			vendor = "Suyog Tandel"
 			licenseFile.set(project.file("../LICENSE"))
