d2a93075341a92d952b188fa4542a4bb6a130833
Suyog Tandel <suyogtandel@proton.me>
2025-11-18 19:00:03 +0530

    chore: remove Zulu repository and fix Temurin JDK 25 directory structure in packaging script

--- packaging/build-scripts/package.sh
+++ packaging/build-scripts/package.sh
@@ -10,9 +10,6 @@ echo "Updating container and installing dependencies..."
 
 dnf update -y
 dnf install -y curl wget git ca-certificates fuse-libs unzip binutils file glib2 findutils
-echo "Adding Azul Zulu repository..."
-rpm --import https://repos.azul.com/azul-repo.key
-curl -s https://repos.azul.com/zulu/zulu-rhel.repo -o /etc/yum.repos.d/zulu.repo
 
 # Install Java 25 using manual download
 echo "Installing Java 25..."
@@ -24,12 +21,12 @@ if [[ "$arch" == x86_64 ]]; then
   wget https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.1%2B8/OpenJDK25U-jdk_x64_linux_hotspot_25.0.1_8.tar.gz
   tar -xzf OpenJDK25U-jdk_x64_linux_hotspot_25.0.1_8.tar.gz
   mkdir -p /usr/lib/jvm/temurin25
-  mv OpenJDK25U-jdk_x64_linux_hotspot_25.0.1_8/* /usr/lib/jvm/temurin25/
+  mv OpenJDK25U-jdk_x64_linux_hotspot_25.0.1_8/jdk-25.0.1+8/* /usr/lib/jvm/temurin25/
 else
   wget https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.1%2B8/OpenJDK25U-jdk_aarch64_linux_hotspot_25.0.1_8.tar.gz
   tar -xzf OpenJDK25U-jdk_aarch64_linux_hotspot_25.0.1_8.tar.gz
   mkdir -p /usr/lib/jvm/temurin25
-  mv OpenJDK25U-jdk_aarch64_linux_hotspot_25.0.1_8/* /usr/lib/jvm/temurin25/
+  mv OpenJDK25U-jdk_aarch64_linux_hotspot_25.0.1_8/jdk-25.0.1+8/* /usr/lib/jvm/temurin25/
 fi
 
 echo "Setting JAVA_HOME and PATH..."
